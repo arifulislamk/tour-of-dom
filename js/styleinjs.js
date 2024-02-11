0738b3ab9e6956a5d02ad5f0669504992d0d5fb2
arifulislamk <ariful18374@gmail.com>
2024-02-12 01:01:27 +0600

    first commit

---
+++ js/styleinjs.js
@@ -0,0 +1,9 @@
+const allsection = document.querySelectorAll('section')
+for(const section of allsection){
+    section.style.fontSize = '20px' ;
+    section.style.border = '5px solid gray';
+    section.style.marginBottom = '10px'
+    section.style.padding = '50px' ;
+    section.style.backgroundColor = 'tomato'
+    section.style.borderRadius = '55px'
+}
---
+++ script/dom.js
@@ -0,0 +1,10 @@
+console.log('heelo dom');
+console.log(document.body);
+const someli = document.querySelectorAll('.fruits-container li');
+ for (const lif of someli){
+   //  console.log(lif.innerText);
+ }
+
+const title = document.getElementById('capital')
+//  console.log(title.getAttribute('class') ) ;
+
